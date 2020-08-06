001159d1eb61cccf749f8687c1832d06efc618e6
Ronnie Sahlberg <ronniesahlberg@gmail.com>
2020-08-06 13:36:42 +1000

    ps2: support for ps2 EE environment
    Signed-off-by: Ronnie Sahlberg <ronniesahlberg@gmail.com>

--- lib/init.c
+++ lib/init.c
@@ -43,8 +43,11 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <sys/socket.h>
 #include <time.h>
+
+#ifndef PS2_EE_PLATFORM
+#include <sys/socket.h>
+#endif
 
 #include "smb2.h"
 #include "libsmb2.h"
@@ -52,6 +55,10 @@
 #include "libsmb2-config.h"
 
 #define MAX_URL_SIZE 256
+
+#ifdef PS2_EE_PLATFORM
+#include "compat.h"
+#endif
 
 #ifdef _MSC_VER
 #include <errno.h>
